d5eb2a03d048b84aad770c0dcc4f9a8751df2d45
Ilya Lityagin <ilityagin@phoenixit.ru>
2022-01-17 14:25:44 +0300

    @version 1.1.2-ru

--- src/main/groovy/com/miku/reminder/ReminderApplication.groovy
+++ src/main/groovy/com/miku/reminder/ReminderApplication.groovy
@@ -28,6 +28,7 @@ class ReminderApplication {
     @EventListener(ContextRefreshedEvent.class)
     initBot(){
         TimeZone.setDefault(TimeZone.getTimeZone("Europe/Moscow"))
+        Locale.setDefault(new Locale("ru", "RU"))
         try {
             Thread thread = new Thread(remindInvoker)
             thread.setDaemon(true)
